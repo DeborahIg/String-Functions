ce3327165c3ce1edec5586877f523fbc64cf3dfd
Abdi~ <kaliq.i@outlook.com>
2021-01-28 15:54:36 +0000

    capitalise and lower

--- src/main.py
+++ src/main.py
@@ -4,3 +4,15 @@ def capitalise_string (string):
 
 capitalise_string('eggs')
 
+
+def decapitalise_string (string):
+    print(string.lower())
+    return string.lower() 
+
+decapitalise_string('EGGS')
+
+def cap_first(string):
+    print(string.capitalize())
+    return string.capitalize() 
+
+cap_first('eggs')
--- src/main_test.py
+++ src/main_test.py
@@ -8,3 +8,18 @@ def test_capitalise_string ():
     assert main.capitalise_string ('bangers and 34 mash!!') == 'BANGERS AND 34 MASH!!' 
 
 
+def test_decapitalise_string ():
+    assert main.decapitalise_string ('EGGS') == 'eggs' 
+    assert main.decapitalise_string ('BACON') == 'bacon' 
+    assert main.decapitalise_string ('SAUSAGE') == 'sausage' 
+    assert main.decapitalise_string ('MILK AND YOGHURT') == 'milk and yoghurt' 
+    assert main.decapitalise_string ('HASUH98£') == 'hasuh98£' 
+
+def test_cap_string ():
+    assert main.cap_first ('eggs') == 'Eggs' 
+    assert main.cap_first ('bacon') == 'Bacon' 
+    assert main.cap_first ('sausage') == 'Sausage' 
+    assert main.cap_first ('my milk') == 'My milk' 
+    assert main.cap_first('ha8$') == 'Ha8$'
+    assert main.cap_first ('8a') == '8a'
+
