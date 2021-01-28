39523feb6ccf6be79b9c86ba14fa10e608e36831
Deborah Igunma <debbieigunma@gmail.com>
2021-01-28 15:42:55 +0000

    added a few more tests

--- src/main_test.py
+++ src/main_test.py
@@ -4,6 +4,7 @@ def test_capitalise_string ():
     assert main.capitalise_string ('eggs') == 'EGGS' 
     assert main.capitalise_string ('bacon') == 'BACON' 
     assert main.capitalise_string ('sausage') == 'SAUSAGE' 
-    
+    assert main.capitalise_string ('eggs benedict') == 'EGGS BENEDICT' 
+    assert main.capitalise_string ('bangers and 34 mash!!') == 'BANGERS AND 34 MASH!!' 
 
 
