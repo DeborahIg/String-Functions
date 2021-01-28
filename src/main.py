8a53c134e91c6aef4e8e576342ef81eb78a616ca
Deborah Igunma <debbieigunma@gmail.com>
2021-01-28 15:00:34 +0000

    tests and stuff

---
+++ src/main.py
@@ -0,0 +1,5 @@
+def capitalise_string (string):
+    print(string)
+    return string 
+capitalise_string('eggs')
+
---
+++ src/main_test.py
@@ -0,0 +1,7 @@
+import main
+
+def test_capitalise_string ():
+    assert main.capitalise_string ('eggs') == 'EGGS' 
+
+# def test_hello_world():
+#   assert main.hello_world() == "Hello, world!"
